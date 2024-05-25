aca2a0b2266af53cf7c13843ccfa6af3bee61106
2651084151 <2651084151@qq.com>
2024-05-25 21:24:21 +0800

    计算机操作系统生产者消费者算法

---
+++ src/DataStructures/Producer.java
@@ -0,0 +1,22 @@
+package DataStructures;
+public class Producer implements Runnable{
+    private Storage storage;
+
+    public Producer(){}
+
+    public Producer(Storage storage){
+        this.storage = storage;
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            try{
+                Thread.sleep(1000);
+                storage.produce();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
